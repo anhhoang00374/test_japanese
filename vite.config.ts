caafb3a40db50e5a48c9b47981afbef098f097c9
anhhoang00374 <ahhoang00374@gmail.com>
2025-10-19 21:35:10 +0700

    ci: auto deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // ⚠️ thay <repo-name> bằng tên repo của bạn
 export default defineConfig({
     plugins: [react()],
-    base: "test_japanese",
+    base: "/test_japanese/",
 });
