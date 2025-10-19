ecd08e55d2990ef0efdc99c9bf39bbe7a8751dc6
anhhoang00374 <ahhoang00374@gmail.com>
2025-10-19 21:26:25 +0700

    ci: auto deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
+// ⚠️ thay <repo-name> bằng tên repo của bạn
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    base: "test_japanese",
+});
